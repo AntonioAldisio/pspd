e49e59fcee74170247737cc1cdf1df9a36441b7a
ANTÔNIO ALDÍSIO DE SOUSA ALVES FERREIRA FILHO <a202028211@cm2.esw>
2023-04-15 21:58:19 -0300

    lab02

--- hadoop/lab_01/redeAmizades/FriendsCount.java
+++ hadoop/lab_01/redeAmizades/FriendsCount.java
@@ -1,6 +1,9 @@
 import java.io.IOException;
+import java.util.StringTokenizer;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -8,44 +11,54 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class MyMapReduce {
-    public static class MyMapper extends Mapper<Object, Text, Text, Text> {
-        private Text entrada = new Text();
-        private Text saida = new Text();
+public class FriendsCount {
 
-        public void map(Object key, Text value, Context context)
-                throws IOException, InterruptedException {
-            String[] valores = value.toString().split(",");
-            if (valores.length == 2) {
-                entrada.set(valores[0]);
-                saida.set(valores[1]);
-                context.write(entrada, saida);
-            }
-        }
-    }
+  public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
 
-    public static class MyReducer extends Reducer<Text, Text, Text, Text> {
-        public void reduce(Text key, Iterable<Text> values, Context context)
-                throws IOException, InterruptedException {
-            StringBuilder sb = new StringBuilder();
-            for (Text value : values) {
-                sb.append(value.toString()).append(" ");
-            }
-            String saidas = sb.toString().trim();
-            context.write(key, new Text(saidas));
-        }
-    }
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
 
-    public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "MyMapReduce");
-        job.setJarByClass(MyMapReduce.class);
-        job.setMapperClass(MyMapper.class);
-        job.setReducerClass(MyReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+
+      String aux = value.toString();
+      int pos = aux.indexOf(",");
+      StringTokenizer itr = new StringTokenizer(aux.substring(0,pos));
+      while (itr.hasMoreTokens()) {
+        word.set(itr.nextToken());
+        context.write(word, one);
+      }
     }
+  }
+
+  public static class IntSumReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "Friends Count");
+    job.setJarByClass(FriendsCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
 }
